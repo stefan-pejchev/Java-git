93020cdb11ea23625ffecddf7ce84055b2aa1721
Stefan Peychev <stefan.pejchev@gmail.com>
2020-03-09 20:59:56 +0200

    Second commit

--- com.stefanpeychev.git.firrst/src/com/stefanpeychev/git/firrst/GitTest.java
+++ com.stefanpeychev.git.firrst/src/com/stefanpeychev/git/firrst/GitTest.java
@@ -2,7 +2,7 @@ package com.stefanpeychev.git.firrst;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git is fun!");
+		System.out.println("Git is cool!");
 	}
 
 }
